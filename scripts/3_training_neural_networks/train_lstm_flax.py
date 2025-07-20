4522ea3f4c0a6be786a6a8b266d2ac54bb09af95
Klemen <klemenpozlep@protonmail.com>
2025-07-20 23:44:23 +0200

    Adding some rough basics IV

--- scripts/3_training_neural_networks/train_lstm_flax.py
+++ scripts/3_training_neural_networks/train_lstm_flax.py
@@ -154,6 +154,26 @@ def eval_step(state, batch):
 ### MAIN EXECUTION BLOCK ###
 # ==============================================================================
 if __name__ == "__main__":
+
+
+    def create_learning_rate_fn(config, base_learning_rate, steps_per_epoch):
+        """ Creates learning rate schedule. """
+        warmup_fn = optax.linear_schedule(
+            init_value=0., end_value=base_learning_rate,
+            transition_steps=config.warmup_epochs * steps_per_epoch)
+        cosine_epochs = max(config.num_epochs - config.warmup_epochs, 1)
+        cosine_fn = optax.cosine_decay_schedule(
+            init_value=base_learning_rate,
+            decay_steps=cosine_epochs * steps_per_epoch)
+        schedule_fn = optax.join_schedules(
+            schedules=[warmup_fn, cosine_fn],
+            boundaries=[config.warmup_epochs * steps_per_epoch])
+        return schedule_fn
+
+
+
+
+
     # --- 1. Setup and Hyperparameters ---
     MAX_LEN = 60         # Maximum length of a tweet sequence
     MAX_FEATURES = 15000 # Maximum number of words in our vocabulary
